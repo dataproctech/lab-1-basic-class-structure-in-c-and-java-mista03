11fe8bd81c52df267cc5a3d3abdc6f95db197eb5
mista03 <michal.stachowiak@student.put.poznan.pl>
2024-10-02 17:53:18 +0200

    fixed duplicate files
    accidentaly copy pasted cpp program into java file

--- Car.java
+++ Car.java
@@ -1,33 +1,25 @@
-// include the necessary libraries
-#include <iostream>
-#include <string>
+public class Car {
+    // Attributes
+    String make;
+    String model;
+    int year;
 
-using namespace std;
+    // Constructor
+    public Car(String x, String y, int z){
+        make = x;
+        model = y;
+        year = z;
+    }
 
-class Car {
-    public:
-        //attributes
-        string make;
-        string model;
-        int year;
+    // Method to display car information
+    public void displayInfo(){
+        System.out.println(make + ", " + model + ", " + year);
+    }
 
-        // Constructor
-        Car(string x, string y, int z){
-            make = x;
-            model = y;
-            year = z;
-        }
-        // Method to display car information
-        void displayInfo(){
-            cout << make << ", " << model << ", " << year << "\n"; 
-        }
-};
-
-int main() {
-    // Create a Car object
-    Car car1("mazda", "rx7", 1999);
-    // Display car details
-    car1.displayInfo();
-
-    return 0;
+    public static void main(String[] args) {
+            // Create a Car object
+            Car car1 = new Car("mazda", "miata", 1999);
+            // Display car details
+            car1.displayInfo();
+    }
 }
