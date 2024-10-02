22c005bddcd3a95b781e9489f9bfff437e3076f4
mista03 <michal.stachowiak@student.put.poznan.pl>
2024-10-02 14:47:56 +0200

    created Car class
    with constructor and displayInfo method

--- main.cpp
+++ main.cpp
@@ -1,18 +1,33 @@
 // include the necessary libraries
+#include <iostream>
+#include <string>
+
+using namespace std;
 
 class Car {
-    //attributes
+    public:
+        //attributes
+        string make;
+        string model;
+        int year;
 
-    // Constructor
-
-    // Method to display car information
-   
+        // Constructor
+        Car(string x, string y, int z){
+            make = x;
+            model = y;
+            year = z;
+        }
+        // Method to display car information
+        void displayInfo(){
+            cout << make << ", " << model << ", " << year << "\n"; 
+        }
 };
 
 int main() {
     // Create a Car object
-    
+    Car car1("mazda", "rx7", 1999);
     // Display car details
-    
+    car1.displayInfo();
+
     return 0;
 }
