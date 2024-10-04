aa6e19af6a3836cc35b634abe2880b13919f00b6
mista03 <michal.stachowiak@student.put.poznan.pl>
2024-10-04 10:52:33 +0200

    Added getCarAge and isSameCar methods

--- Car.java
+++ Car.java
@@ -3,6 +3,7 @@ public class Car {
     String make;
     String model;
     int year;
+    int currentYear = 2024;
 
     // Constructor
     public Car(String x, String y, int z){
@@ -10,16 +11,35 @@ public class Car {
         model = y;
         year = z;
     }
-
+    
     // Method to display car information
     public void displayInfo(){
         System.out.println(make + ", " + model + ", " + year);
     }
+    
+    public int getCarAge(){
+        return currentYear - year;
+    }
+    
+    public static boolean isSameCar(Car car1, Car car2){
+        if(car1.make != car2.make) return false;
+        if(car1.model != car2.model) return false;
+        if(car1.year != car2.year) return false;
+        return true;
+    }
 
     public static void main(String[] args) {
             // Create a Car object
-            Car car1 = new Car("mazda", "miata", 1999);
+            Car car1 = new Car("mazda", "rx7", 1999);
+            Car car2 = new Car("mazda", "miata", 1999);
+            
             // Display car details
             car1.displayInfo();
+            System.out.println(car1.getCarAge());
+            car2.displayInfo();
+            System.out.println(car2.getCarAge());
+            
+            System.out.println(isSameCar(car1, car2));
+            
     }
 }
